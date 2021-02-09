ec1970e27e6e65a75d9bac574eed98db671df024
Ivan <example@gmail.com>
2021-02-09 20:22:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// "use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
+// console.log(number)
+
+// const obj = {
+//     a: 10
+// }
+// obj.a = 5; 
+// console.log(obj); 
+// alert('hello')
+// const result = confirm("r u here");
+// console.log(result);
+// const answer = prompt("Vam est 18?","18 ")
+// console.log(typeof(answer));
+
+// const answers = [];
+// // answers[0] = prompt('kak vashe imy?', '');
+// // answers[1] = prompt('kak vashe family?', '');
+// // answers[2 ] = prompt('skok vam let?', '');
+// // document.write(answers);
+// console.log(typeof(answers));
+ const category = 'toys';
