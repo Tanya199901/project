b20a67e8b9bc663c912569f58764b16b9d0ba2b9
Ivan <example@gmail.com>
2021-02-10 21:19:55 +0300

    12 video save

--- js/script.js
+++ js/script.js
@@ -22,4 +22,18 @@
 // // answers[2 ] = prompt('skok vam let?', '');
 // // document.write(answers);
 // console.log(typeof(answers));
- const category = 'toys';
+ const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+ const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+ }; 
+ const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На сколько оцените его', ''),
+       c =  prompt('Один из последних просмотренных фильмов?', ''),
+       d = prompt('На сколько оцените его', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
