6b9f4fb7ce91739559f26d9c7e64040148281785
Ivan <example@gmail.com>
2021-02-10 21:49:58 +0300

    13 video

--- js/script.js
+++ js/script.js
@@ -22,18 +22,40 @@
 // // answers[2 ] = prompt('skok vam let?', '');
 // // document.write(answers);
 // console.log(typeof(answers));
- const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
- const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
- }; 
- const a = prompt('Один из последних просмотренных фильмов?', ''),
-       b = prompt('На сколько оцените его', ''),
-       c =  prompt('Один из последних просмотренных фильмов?', ''),
-       d = prompt('На сколько оцените его', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+// 12 video
+//  const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//  const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+//  }; 
+//  const a = prompt('Один из последних просмотренных фильмов?', ''),
+//        b = prompt('На сколько оцените его', ''),
+//        c =  prompt('Один из последних просмотренных фильмов?', ''),
+//        d = prompt('На сколько оцените его', '');
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+
+// if (4 == 5) {
+//      console.log('ok!'); 
+// } else {
+//     console.log('error'); 
+// }
+const num = 50;
+switch (num) {
+    case 49:
+      console.log('neverno');
+    break;
+    case 100 :
+        console.log('neverno');
+      break;
+      case 50:
+        console.log('verno');
+      break;
+      default:
+        console.log('ne v etot raz');
+        break; 
+}
